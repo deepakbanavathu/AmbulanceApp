94018e718c3283db749d3cec9c1d9bca9dcad086
deepak banavathu <deepak.b7194@gmail.com>
2025-08-31 21:10:33 +0530

    Including the Jwt token in the intial change .

--- ambulance-app_backend/Ambulance.Api/Program.cs
+++ ambulance-app_backend/Ambulance.Api/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,14 +12,35 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
 builder.Services.AddCors(options =>{
     options.AddPolicy("AllowAngular",
     policy => policy.withOrigins("https://localhost:4200")
                     .AllowAnyHeader()
                     .AllowAnyMethod());
 })
+
+//Add Authentication
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDeafaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDeafaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>{
+    var jwtSettings = builder.Configuration.getSection("Jwt");
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSingingKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["key"])),
+    };
+});
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
